d5b3713e193258b035a047806290d7cb0eff4b3f
Tyler OBrien <tylerob65@gmail.com>
2023-03-23 22:47:23 -0400

    first commit

---
+++ parkingGarage.py
@@ -0,0 +1,34 @@
+
+# # # Brainstorming area
+
+# Parking Garage Class
+    # Thing to track
+        # Total Spaces
+        # Occupied Spaces
+        # Cost
+
+    # Things we care about
+    
+    # Functions
+        # Enter Garage
+        
+        # Pay ticket
+        # Only can if ticket has been paid
+        # Print current occupancy
+        # CheckIfOpen
+            # Visitor calls this
+            # If opening get ticket
+
+# When you 
+
+
+# Sudo code
+
+tickets = [0,1,2,3,4,5,6,7,8,9]
+
+class Garage():
+    def __init__(self):
+        self.TotalSpaces = 10
+        
+    
+    pass
